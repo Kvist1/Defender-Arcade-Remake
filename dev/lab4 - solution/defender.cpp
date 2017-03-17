6bde302c7c778316f9a7538c32c3a015b686288e
Kvist2 <simon_lindkvist92@hotmail.com>
2017-03-17 15:17:05 +0100

    cleanup

--- dev/lab4 - solution/defender.cpp
+++ dev/lab4 - solution/defender.cpp
@@ -1,11 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "stdio.h"
-#include "avancezlib.h"
-#include "object_pool.h"
-
-#include <set>
-
 #include "GlobalVariables.h"
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
@@ -19,7 +13,6 @@ const int MINIMAP_HEIGHT = LEVEL_HEIGHT/6;
 const unsigned int	MAX_NUM_GAME_OBJECTS = 10000;
 const unsigned int	NUM_LIVES = 5;
 const unsigned int	NUM_PLAYER_BOMBS = 3;
-
 const unsigned int	MAX_NUM_ROCKETS = 32;
 const unsigned int	MAX_NUM_BOMBS = 32;
 const unsigned int	POINTS_PER_ALIEN = 100;
@@ -33,17 +26,15 @@ const float			ALIEN_SPEED = 160.0f;
 const float			BOMB_SPEED = 500.0f;
 const float			HUMAN_SPEED = 30.0f;
 
+float GAME_SPEED = 1.f;		// speed of the game; it is also the score multiplier
 
-float GAME_SPEED = 1.f;		// speed of the game; it is increased each time all the aliens are hit
-							// it is also the score multiplier
-
+#include "stdio.h"
+#include "avancezlib.h"
+#include "object_pool.h"
+#include <set>
 
 #include "component.h"
 #include "game_object.h"
-
-//#include "collision_system.h"
-
-
 #include "rocket.h"
 #include "bomb.h"
 #include "player.h"
